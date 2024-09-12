f0c7261a12fc9973a5a615bc8e1073f9048ef09e
MehceUnisen <mehceu@gmail.com>
2024-09-12 23:55:25 +0300

    automatic

--- lipo.m
+++ lipo.m
@@ -3,11 +3,10 @@
 % Parameters
 capacity = 2000; % Battery capacity in mAh (milliampere-hour)
 initial_SOC = 50; % Initial State of Charge in percentage (0-100)
-charge_C_rate = 1; % Charging rate in C-rate (e.g., 1C means full charge in 1 hour)
-discharge_C_rate = 0.5; % Discharging rate in C-rate (e.g., 0.5C means full discharge in 2 hours)
+charge_C_rate = 2; % Charging rate in C-rate (e.g., 1C means full charge in 1 hour)
+discharge_C_rate = 1; % Discharging rate in C-rate (e.g., 0.5C means full discharge in 2 hours)
 time_step = 1; % Time step in minutes
 total_time = 240; % Total simulation time in minutes
-charge_efficiency = 0.95; % Charging efficiency (95%)
 
 % Convert capacity from mAh to Ah for calculations
 capacity_Ah = capacity / 1000; 
@@ -26,7 +25,7 @@ for i = 1:length(time)
     % Check if charging or discharging
     if mod(time(i), 60) < 30 % Example: charge for 30 minutes every hour
         % Charging
-        SOC = SOC + (charge_C_rate * capacity_Ah * (time_step / 60)) * charge_efficiency / capacity_Ah * 100;
+        SOC = SOC + (charge_C_rate * capacity_Ah * (time_step / 60))  / capacity_Ah * 100;
     else
         % Discharging
         SOC = SOC - (discharge_C_rate * capacity_Ah * (time_step / 60)) / capacity_Ah * 100;
